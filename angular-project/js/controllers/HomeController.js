9f54387fe09f6a411462126ca814c0659370b7df
Naeem Rahman <Naeem@MacBook-Air.local>
2016-08-20 22:55:39 +0100

    commit #3 Angular-route not working correctly

--- angular-project/js/controllers/HomeController.js
+++ angular-project/js/controllers/HomeController.js
@@ -1,25 +1,12 @@
 app.controller('HomeController', ['$scope', 'data', function($scope, data){
   $scope.helloWorld = "Hello, Naeem Rahman! You are using Anglular!";
-
   $scope.posts = data.suggestions;
 
-  $scope.errorNameCheck = "Naeem";
-  $scope.errorSuggestionCheck = "Rahmsn";
 
-  $scope.addSuggestion = function(){
-    console.log("bad feed");
-    if ($scope.message == ''){
-      $scope.errorSuggestionCheck = "Woops! You forgot to enter your suggestion!";
-      return;
-    };
-    if ($scope.from == '') {
-      $scope.errorNameCheck = "Woops! You forgot to enter yourn name";
-      return;
-    }
-
-    $scope.posts.push({
-
-    })
+  $scope.upVote = function(post){
+    console.log("I AM RUNNING CONTROLLER");
+    post.upvotes += 1;
   }
+
 
 }]);
