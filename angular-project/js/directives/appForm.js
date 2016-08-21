d85e5bf931a69c647563d0fc02dc019b467c882b
Naeem Rahman <Naeem@MacBook-Air.local>
2016-08-21 23:17:39 +0100

    commit #5 how to connect multiple views to one directive and update template data accordingly

--- angular-project/js/directives/appForm.js
+++ angular-project/js/directives/appForm.js
@@ -1,14 +1,29 @@
-app.directive('appForm', function(){
+app.directive('appForm', ['$location',function(location){
   return {
     restrict: 'E',
     scope: {
-      info: '='
+      info: '=',
+      information: '='
     },
     templateUrl: 'js/directives/formTemplate.html',
     link: function(scope, elem, attr){
       console.log(scope);
       scope.errorNameCheck = "";
       scope.errorSuggestionCheck = "";
+
+
+      console.log(location.path());
+
+      if (location.path() == '/'){
+        console.log("HEY");
+        scope.Title = "Submit Your Suggestion";
+      }
+      else if (location.path().includes('/comments/')) {
+        console.log("YOUR IN THE COMMENTS PAGE")
+        scope.Title = "Submit Your Comment";
+      }
+
+
 
       scope.addSuggestion = function(){
         if (typeof scope.message == "undefined" | scope.message == ''){
@@ -31,4 +46,4 @@ app.directive('appForm', function(){
     }
   }
 }
-});
+}]);
