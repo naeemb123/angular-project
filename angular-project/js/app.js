c86a21d946c3faf94dcf34b49ebddd7aa6b37bbd
Naeem Rahman <Naeem@MacBook-Air.local>
2016-08-21 15:09:49 +0100

    commit #4 post is not connected to its right

--- angular-project/js/app.js
+++ angular-project/js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('SuggestionBoxApp', []);
+var app = angular.module('SuggestionBoxApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    controller: 'HomeController',
+    templateUrl: './views/home.html'
+  })
+  .when('/comments/:id', {
+    controller: 'CommentController',
+    templateUrl: './views/commentPage.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+})
