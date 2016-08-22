37ce1ac4bda5d64f19636b0d2588c9bb35e3ec4b
naeemb123 <naeemb_2k7@hotmail.com>
2016-08-22 20:55:25 +0100

    #6 How to get form-inline and update data in my view according to controller data

--- angular-project/js/controllers/CommentController.js
+++ angular-project/js/controllers/CommentController.js
@@ -1,5 +1,15 @@
 app.controller('CommentController', ['$scope', '$routeParams', 'data', function($scope, $routeParams, data){
   $scope.post = data.suggestions[$routeParams.id];
   $scope.currentPostIndex = parseInt($routeParams.id);
-  console.log($scope.currentPostIndex);
+
+
+  $scope.emptyComments = "";
+  if($scope.post.comments.length == 0) {
+    $scope.emptyComments = "Make The First Comment !";
+    console.log('running?');
+  }
+  else {
+    $scope.emptyComments = "";
+  }
+
 }]);
