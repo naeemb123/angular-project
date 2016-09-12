f4755b6fba5cdb552b8fbb43d88f64ca17efe58a
naeemb123 <naeemb_2k7@hotmail.com>
2016-09-12 05:58:19 +0100

    Final Commit - Project Complete

--- angular-project/js/app.js
+++ angular-project/js/app.js
@@ -4,11 +4,11 @@ app.config(function($routeProvider){
   $routeProvider
   .when('/', {
     controller: 'HomeController',
-    templateUrl: './views/home.html'
+    templateUrl: 'views/home.html'
   })
   .when('/comments/:id', {
     controller: 'CommentController',
-    templateUrl: './views/commentPage.html'
+    templateUrl: 'views/commentPage.html'
   })
   .otherwise({
     redirectTo: '/'
